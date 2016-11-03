fb8a5fc9f9e5ff4e1d22ea6581a665d3dd73cd18
pixelsquaremarketing <estudo@pixelsquaremarketing.com>
2016-11-03 09:38:19 -0200

    Subindo o Projeto
    Subindo o projeto.

---
+++ app/src/main/java/firebase/com/pixelsquare/firebase/MainActivity.java
@@ -0,0 +1,17 @@
+package firebase.com.pixelsquare.firebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatabaseReference db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
